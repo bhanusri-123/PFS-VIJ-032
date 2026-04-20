70e5788c083c7d37bd132e62bb8a3f55491de2c5
Venigalla Bhanusri <163542770+bhanusri-123@users.noreply.github.com>
2026-04-20 17:03:43 +0530

    Add files via upload

---
+++ conditions(multiple-if).py
@@ -0,0 +1,73 @@
+#multiple-if conditions using comparison operators
+'''a = 9
+b = 4
+if a>b:
+    print("greater")
+if a==b:
+    print("Equal")
+if a!=b:
+    print("Not equal")
+if b<a:
+    print("Less")'''
+
+'''a = 20
+b = 35
+if a==b:
+    print("equal")
+if a!=b:
+    print("Not equal")'''
+
+#multiple-if conditions using logical operators
+
+'''a = 9
+b = 4
+if a>b and b<a:
+    print("Condition satisfied")
+if a==b or a!=b:
+    print("True")
+if not a<b or a==b:
+    print("Not condition")'''
+
+'''a = 20
+b = 35
+if not (a<b or a==b):
+    print("not condition")
+if a!=b and b>a:
+    print("and condition")'''
+
+#multiple-if conditions using identity operators
+
+'''a = 9
+b = 9.3
+if type(a) is int and type(b) is float:
+    print("True")
+if type(a) is not float:
+    print("Not float")
+if type(b) is int:
+    print("False")'''
+
+'''a = 10.5
+if type(a) is not float:
+    print("Not float")
+if type(a) is float:
+    print("Float")'''
+
+#multiple-if conditions using membership operators
+
+'''a = [10,20,40,60,80,25]
+if 10 in a:
+    print("10 in a")
+if 28 not in a:
+    print("28 not in a")
+if 40 not in a:
+    print("40 not in a")
+if 60 in a:
+    print("60 in a")'''
+
+'''a = [88,20,10,40,22,33,90]
+if 10 in a:
+    print("10 in a")
+if 25 not in a:
+    print("25 not in a")'''
+
+
---
+++ conditions(nested-if).py
@@ -0,0 +1,57 @@
+#nested-if conditions
+
+'''a = 5
+b = 10
+if a<b:
+    print("Less")
+    if b>a:
+        print("Greater")'''
+
+'''a = 5
+b = 10
+if a>b:
+    print("less")
+    if b==a:
+        print("Equal")'''
+
+'''a = 10
+b = 20
+if a>b:
+    print("Less")
+    if b>a:
+        print("Greater")
+else:
+    print("Condition not satisfied")'''
+
+'''a = 5
+b = 10
+if a<b:
+    print("Less")
+    if b<a:
+        print("Greater")
+    else:
+        print("True")'''
+
+a = 5
+b = 10
+if a<b:
+    print("Less")
+    if b>a:
+        print("Greater")
+    else:
+        print("False")
+else:
+    print("Condition not satisfied")
+
+'''a = 7
+b = 12
+if a<b:
+    print("Less")
+    if a==b:
+        print("Equal")
+    elif a!=b:
+        print("Not Equal")
+    else:
+        print("False")
+else:
+    print("Condition not satisfied")'''
